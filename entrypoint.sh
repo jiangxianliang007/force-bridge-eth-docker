ecdbdf9861aa5c68333e30c98e5f37c5fd561590
JiangXL <jiangxianliang@cryptape.com>
2021-01-29 13:32:00 +0800

    add --ckb-indexer-url

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 DB_NAME=forcedb
 DB_PATH=mysql://root:root@127.0.0.1:3306/${DB_NAME}
+INDEXER_URL=http://127.0.0.1:8116
 HEADER_RELAY_PRIVKEY=1
 CKB_MINT_PRIVKY=2
 ETH_UNLOCK_PRIVKEY=2
@@ -13,16 +14,16 @@ ckb-header-relay() {
  force-eth-cli ckb-relay --config-path ./config.toml -k ${HEADER_RELAY_PRIVKEY} --per-amount 10  --max-tx-count 10 --mutlisig-privkeys  0
 }
 eth-header-relay() {
- force-eth-cli eth-relay --config-path ./config.toml -k ${HEADER_RELAY_PRIVKEY} --multisig-privkeys 1
+ force-eth-cli eth-relay --config-path ./config.toml -k ${HEADER_RELAY_PRIVKEY} --multisig-privkeys 0 1
 }
 force-server() {
   RUST_LOG=error,force=info RUST_TRACEBACK=1 force-eth-cli dapp server --config-path ./config.toml --ckb-private-key-path ${CKB_MINT_PRIVKY}  --listen-url 0.0.0.0:3003 --db-path ${DB_PATH}
 }
 ckb-indexer() {
- force-eth-cli dapp ckb-indexer --config-path ./config.toml --db-path ${DB_PATH}
+ force-eth-cli dapp ckb-indexer --config-path ./config.toml --db-path ${DB_PATH} --ckb-indexer-url ${INDEXER_URL}
 }
 eth-indexer() {
- force-eth-cli dapp eth-indexer --config-path ./config.toml --db-path ${DB_PATH}
+ force-eth-cli dapp eth-indexer --config-path ./config.toml --db-path ${DB_PATH} --ckb-indexer-url ${INDEXER_URL}
 }
 ckb-tx-relayer() {
  force-eth-cli dapp ckb-tx-relayer --config-path ./config.toml --db-path ${DB_PATH} -k ${ETH_UNLOCK_PRIVKEY}
